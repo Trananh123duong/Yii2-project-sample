db1d0fe03d9b5c37110ce850b6223017eaa241e0
Duong <trananh123duong@gmail.com>
2023-09-13 09:41:42 +0700

    create table by migration

---
+++ migrations/m230913_020347_create_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m230913_020347_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'password' => $this->string()->notNull(),
+            'name' => $this->string(),
+            'email' => $this->string()->notNull()->unique(),
+            'role' => $this->smallInteger()->notNull(),
+            'description' => $this->text(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
